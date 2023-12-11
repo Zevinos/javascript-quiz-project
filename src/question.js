a2da536b6365df867450ca8cd7dc31473de2338e
evanc <evan.coindreau@gmail.com>
2023-12-11 16:00:10 +0100

    reset

--- src/question.js
+++ src/question.js
@@ -1,25 +1,6 @@
 class Question {
-	// YOUR CODE HERE:
-	//
-	// 1. constructor (text, choices, answer, difficulty)
-	// 2. shuffleChoices()
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  // 2. shuffleChoices()
 }
-console.log("Huhu")
-console.log("Huhu")
-console.log("Huhu")
-console.log("Huhu")
-console.log("Huhu")
-console.log("Huhu")
-console.log("Huhu")
-
-console.warn("Way too many logs right there.")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
-console.log("oui")
