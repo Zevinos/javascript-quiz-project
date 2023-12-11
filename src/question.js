147ce5e9f07c5da9bc5dc8ce9ef7708e30d5e2bb
evanc <evan.coindreau@gmail.com>
2023-12-11 18:13:10 +0100

    Day 1 finished

--- src/question.js
+++ src/question.js
@@ -1,6 +1,18 @@
 class Question {
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+
   // YOUR CODE HERE:
-  //
-  // 1. constructor (text, choices, answer, difficulty)
-  // 2. shuffleChoices()
+  shuffleChoices() {
+    for (var i = this.choices.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp;
+    }
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 4. shuffleQuestions()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 5. checkAnswer(answer)
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 6. hasEnded()
+  shuffleQuestions() {
+    for (var i = this.questions.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
+    }
+  }
+
+  checkAnswer(answer) {
+    this.correctAnswers++;
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    } else return false;
+  }
 }
